c974fdf4ee2023198e08ee74fb0ac3bf68bd530e
tardis <14971857@qq.com>
2025-01-09 21:23:10 +0800

    test: test

---
+++ clouds/test/tencent/cdn/index.ts
@@ -0,0 +1,19 @@
+import { purgePathCache } from '@tardis-ksh/tencent/cdn';
+import * as process from 'node:process';
+
+async function test() {
+  try {
+    const result = await purgePathCache({
+      secretId: process.env.TENCENT_SECRET_ID!,
+      secretKey: process.env.TENCENT_SECRET_KEY!,
+      paths: ['https://api.ksh7.com/'],
+      flushType: 'delete',
+    });
+
+    console.log(result, 'result');
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+test();
--- clouds/test/test.js
+++
@@ -1,3 +0,0 @@
-import { purgePathCache } from '@tardis-ksh/tencent/cdn';
-
-console.log(purgePathCache);
